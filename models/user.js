f6a9ff4043eb9c27b28f2903057fcc275d4e59db
Dinesh Kumar <dk6089567@gmail.com>
2021-01-04 23:55:13 +0530

    updates2

--- models/user.js
+++ models/user.js
@@ -19,6 +19,8 @@ const UserSchema = new Schema ({
 UserSchema.plugin(passportLocalMongoose);
 
 
-var User = mongoose.models.User || mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User',UserSchema);
 
-module.exports = User;
+// var User = mongoose.models.User || mongoose.model('User', UserSchema);
+
+// module.exports = User;
